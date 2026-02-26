97d4b9b708206ee5d3bb4b581735493106c871da
danieloscar1808 <danieloscar1808@gmail.com>
2026-02-26 12:23:47 -0300

    refresh build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,11 @@ import type { Config } from "tailwindcss";
 
 export default {
   darkMode: ["class"],
-  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  content: ["./index.html",
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx,js,jsx,css}"],
   prefix: "",
   theme: {
     container: {
