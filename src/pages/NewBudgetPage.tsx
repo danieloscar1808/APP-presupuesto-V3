1bb64f7563041d968f38e9c6a7f27cfcfb1d71cf
danieloscar1808 <danieloscar1808@gmail.com>
2026-02-27 11:10:23 -0300

    l

--- src/pages/NewBudgetPage.tsx
+++ src/pages/NewBudgetPage.tsx
@@ -33,7 +33,7 @@ const NewBudgetPage = () => {
 
   const [budget, setBudget] = useState<Partial<Budget>>({
     id: uuid(),
-    number: generarNumeroPresupuesto(),  // ← OBLIGATORIO
+    //number: generarNumeroPresupuesto(),  // ← OBLIGATORIO
     category: undefined,
     clientId: '',
     clientName: '',
