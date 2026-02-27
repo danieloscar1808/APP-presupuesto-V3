d2dc6ddc42364593ef06f3fbe01d38d63269cb66
danieloscar1808 <danieloscar1808@gmail.com>
2026-02-27 09:32:31 -0300

    log

--- src/lib/pdfGenerator.ts
+++ src/lib/pdfGenerator.ts
@@ -27,7 +27,7 @@ export const generateBudgetPDF = (budget: Budget, profile: Profile): jsPDF => {
   // LOGO en el encabezado
   const logoWidth = 38;
   const logoHeight = 0;
-  const logoX = 105;
+  const logoX = 110;
   const logoY = 25;
 
   doc.addImage(logoHeader, 'PNG', logoX, logoY, logoWidth, logoHeight);
