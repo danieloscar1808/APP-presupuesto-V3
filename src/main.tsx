32c8e3266a931bf15989423db12871f7af164695
danieloscar1808 <danieloscar1808@gmail.com>
2026-03-03 15:37:11 -0300

    de

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,14 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import DebugDBPage from "./pages/DebugDBPage"; 
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/APP-presupuesto-V3">
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/debugdb" element={<DebugDBPage />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
